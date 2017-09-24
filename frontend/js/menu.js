05d53982a25859bc9b65c1e550e0faddca1a7002
Yakovlev Pavel <py@theinvaders.pro>
2017-09-24 14:39:28 +0700

    fix link menu to js

--- frontend/js/menu.js
+++ frontend/js/menu.js
@@ -43,7 +43,7 @@
             }
         });
 
-        $(".menu-anchor").on("click", "a", function (event) {
+        $(".menu-anchor").on("click", ".items-menu", function (event) {
             event.preventDefault();
             let id = $(this).attr('href'), top = $(id).offset().top;
             $('body,html').animate({scrollTop: top}, 500);
